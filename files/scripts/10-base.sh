558e4ba0993f7e10e49d873a6b1831a66b56340e
Esteve Fernandez <33620+esteve@users.noreply.github.com>
2025-07-15 12:02:23 +0200

    Update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -7,7 +7,7 @@ set -xeuo pipefail
 # Examples:
 # dnf install -y 'dnf-command(config-manager)'
 # dnf config-manager --set-enabled crb
-sudo dnf config-manager --add-repo https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
-sudo dnf install tailscale
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
+dnf install tailscale
 
-sudo systemctl enable tailscaled
+systemctl enable tailscaled
